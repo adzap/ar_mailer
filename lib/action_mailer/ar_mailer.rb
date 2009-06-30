93df899bc0d4082b797a28ef34a959be44f389a6
Adam Meehan <adam.meehan@gmail.com>
2009-06-30 18:54:55 +1000

    remove deprecated ActionMailer::ARMailer class

--- lib/action_mailer/ar_mailer.rb
+++ lib/action_mailer/ar_mailer.rb
@@ -1,17 +1,7 @@
-require 'action_mailer'
-
 ##
 # Adds sending email through an ActiveRecord table as a delivery method for
 # ActionMailer.
 #
-
-class ActionMailer::ARMailer < ActionMailer::Base
-
-  def self.inherited(sub)
-    logger.warn('The ActionMailer::ARMailer class has been deprecated. Will be removed in version 2.1. Just use ActionMailer::Base.')
-  end
-
-end
 
 class ActionMailer::Base
 
