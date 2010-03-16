0c2131caf373094d311b96cf81b7c1f9d8df0fd2
laserlemon <steve.richert@gmail.com>
2010-03-16 14:49:56 -0400

    Ensured that mail headers that are intended to be hidden are removed before being saved to the email record.
    This fixes a bug in which BCC addresses were visible to all recipients.

--- lib/action_mailer/ar_mailer.rb
+++ lib/action_mailer/ar_mailer.rb
@@ -23,8 +23,10 @@ class ActionMailer::Base
   # used.
 
   def perform_delivery_activerecord(mail)
+    destinations = mail.destinations
+    mail.ready_to_send
     sender = (mail['return-path'] && mail['return-path'].spec) || mail.from.first
-    mail.destinations.each do |destination|
+    destinations.each do |destination|
       self.class.email_class.create :mail => mail.encoded, :to => destination, :from => sender
     end
   end
